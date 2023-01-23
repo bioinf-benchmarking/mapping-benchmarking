f20d0354c912ad3a5df7d3d262b9b66683270e66
Ivar Grytten <ivar.grytten@gmail.com>
2023-01-23 10:04:13 +0100

    Paired end bwa working

--- scripts/assign_ids_to_sam.py
+++ scripts/assign_ids_to_sam.py
@@ -1,12 +1,29 @@
 # Assigns numeric incremental IDs to a fq file
-
+import logging
+logging.basicConfig(level=logging.INFO)
 import sys
+
 read_id = 0
-for i, line in enumerate(sys.stdin):
+is_paired_end = False
+i = 0
+for line in sys.stdin:
     if line.startswith("@"):
         continue
 
     line = line.strip().split("\t")
-    line[0] = str(read_id)
+    if line[6] != "*":
+        if not is_paired_end:
+            logging.info("Assuming reads are paired end")
+        is_paired_end = True
+
+    read_name = str(read_id)
+    if is_paired_end:
+        read_name += "/" + str((i % 2)+1)
+
+    line[0] = str(read_name)
     print("\t".join(line))
-    read_id += 1
+
+    if not is_paired_end or i % 2 == 1:
+        read_id += 1
+
+    i += 1
